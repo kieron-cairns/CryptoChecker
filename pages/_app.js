f655a15a33bc2b922b51b0e3bc4b637d7a028410
kieron-cairns <kieroncairns@protonmail.com>
2021-07-27 20:45:02 +0100

    _App.js code comments

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,10 @@ function MyApp({ Component, pageProps }) {
   return (
     
     //Global Config for SWR - referenced from https://swr.vercel.app/docs/global-configuration
+    //The global configuration consists of the first part of the url for the API call. 
+    //the config can be called as a component in different areas of our application, along with the 
+    //second part of the url.
+         
     <SWRConfig
       value={{
         //set default fetcher
